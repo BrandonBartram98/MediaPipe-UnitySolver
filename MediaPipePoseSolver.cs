58e5ffdfd420b3600c0c7599eba0fc944ae9230d
Brandon Bartram <35037470+BrandonBartram98@users.noreply.github.com>
2022-10-19 20:20:44 +0100

    Add files via upload

---
+++ MediaPipePoseSolver.cs
@@ -0,0 +1,189 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mediapipe.Unity.CoordinateSystem;
+using Mediapipe.Unity.Holistic;
+using Google.Protobuf;
+using Mediapipe;
+using Mediapipe.Unity;
+
+public class MediaPipePoseSolver : MonoBehaviour
+{
+    public struct Pose
+    {
+        public Arm LeftArm;
+        public Arm RightArm;
+        public Leg LeftLeg;
+        public Leg RightLeg;
+        public Vector3 Spine;
+        public Hips Hips;
+    }
+
+    public struct Arm
+    {
+        public Vector3 Upper;
+        public Vector3 Lower;
+        public Vector3 Hand;
+    }
+
+    public struct Leg
+    {
+        public Vector3 Upper;
+        public Vector3 Lower;
+    }
+
+    public struct Hips
+    {
+        public Vector3 WorldPosition;
+        public Vector3 Position;
+        public Vector3 Rotation;
+    }
+
+    public Pose Solve(NormalizedLandmarkList normalizedLandmarkList)
+    {
+        (Arm leftArm, Arm rightArm) = CalculateArms(normalizedLandmarkList);
+        (Hips hips, Vector3 spine) = CalculateHips(normalizedLandmarkList);
+
+        var pose = new Pose
+        {
+            LeftArm = leftArm,
+            RightArm = rightArm,
+            Hips = hips,
+            Spine = spine
+        };
+
+        return pose;
+    }
+
+    public Vector3 ToVector(NormalizedLandmark landmark) => new(landmark.X, landmark.Y, landmark.Z);
+    public Vector2 ToVector2(Vector3 vector) => new (vector.x, vector.y );
+
+    private (Arm, Arm) CalculateArms(NormalizedLandmarkList normalizedLandmarkList)
+    {
+        var landmarks = normalizedLandmarkList.Landmark;
+
+        var rightArm = new Arm();
+        var leftArm = new Arm();
+
+        rightArm.Upper = VectorExtensions.FindRotation(ToVector(landmarks[11]), ToVector(landmarks[13]), true);
+        leftArm.Upper = VectorExtensions.FindRotation(ToVector(landmarks[12]), ToVector(landmarks[14]), true);
+        rightArm.Upper.y = VectorExtensions.AngleBetween3DCoords(ToVector(landmarks[12]), ToVector(landmarks[11]), ToVector(landmarks[13]));
+        leftArm.Upper.y = VectorExtensions.AngleBetween3DCoords(ToVector(landmarks[11]), ToVector(landmarks[12]), ToVector(landmarks[14]));
+
+        rightArm.Lower = VectorExtensions.FindRotation(ToVector(landmarks[13]), ToVector(landmarks[15]), true);
+        leftArm.Lower = VectorExtensions.FindRotation(ToVector(landmarks[14]), ToVector(landmarks[16]), true);
+        rightArm.Lower.y = VectorExtensions.AngleBetween3DCoords(ToVector(landmarks[11]), ToVector(landmarks[13]), ToVector(landmarks[15]));
+        leftArm.Lower.y = VectorExtensions.AngleBetween3DCoords(ToVector(landmarks[12]), ToVector(landmarks[14]), ToVector(landmarks[16]));
+        rightArm.Lower.z = Math.Clamp(rightArm.Lower.z, -2.14f, 0f);
+        leftArm.Lower.z = Math.Clamp(leftArm.Lower.z, -2.14f, 0f);
+
+        rightArm.Hand = VectorExtensions.FindRotation(ToVector(landmarks[15]), Vector3.Lerp(ToVector(landmarks[17]), ToVector(landmarks[19]), .5f), true);
+        leftArm.Hand = VectorExtensions.FindRotation(ToVector(landmarks[16]), Vector3.Lerp(ToVector(landmarks[18]), ToVector(landmarks[20]), .5f), true);
+
+        // Modify rotations slightly for more natural movement
+        RigArm(ref rightArm, "right");
+        RigArm(ref leftArm, "left");
+
+        return (leftArm, rightArm);
+    }
+
+    private void RigArm(ref Arm arm, string side)
+    {
+        float invert = side == "right" ? 1f : -1f;
+
+        print(invert);
+
+        arm.Upper.z *= -2.3f * invert;
+
+        arm.Upper.y *= MathF.PI * invert;
+        arm.Upper.y -= Math.Max(arm.Lower.x, 0);
+        arm.Upper.y -= -invert * Math.Max(arm.Lower.z, 0);
+        arm.Upper.x -= 0.3f * invert;
+
+        arm.Lower.z *= -2.14f * invert;
+        arm.Lower.y *= 2.14f * invert;
+        arm.Lower.x *= 2.14f * invert;
+
+        // Clamp values to realistic humanoid limits
+        arm.Upper.x = Math.Clamp(arm.Upper.x, -0.5f, MathF.PI);
+        arm.Lower.x = Math.Clamp(arm.Lower.x, -0.3f, 0.3f);
+
+        arm.Hand.y = Math.Clamp(arm.Hand.z * 2, -0.6f, 0.6f); // sides
+        arm.Hand.z = arm.Hand.z * -2.3f * invert; // up and down
+    }
+
+    private (Hips, Vector3) CalculateHips(NormalizedLandmarkList list)
+    {
+        var landmarks = list.Landmark;
+
+        // Find 2D normalized hip and shoulder joint positions / distances
+        Vector2 hipLeft2d = ToVector2(ToVector(landmarks[23]));
+        Vector2 hipRight2d = ToVector2(ToVector(landmarks[24]));
+        Vector2 shoulderLeft2d = ToVector2(ToVector(landmarks[11]));
+        Vector2 shoulderRight2d = ToVector2(ToVector(landmarks[12]));
+
+        Vector2 hipCenter2d = Vector2.Lerp(hipLeft2d, hipRight2d, 1);
+        Vector2 shoulderCenter2d = Vector2.Lerp(shoulderLeft2d, shoulderRight2d, 1);
+        float spineLength = Vector2.Distance(hipCenter2d, shoulderCenter2d);
+
+        var hips = new Hips
+        {
+            Position = new Vector3(Math.Clamp(-1 * (hipCenter2d.x - .65f), -1, 1), 0, Math.Clamp(spineLength - 1, -2, 0)),
+            Rotation = VectorExtensions.RollPitchYaw(ToVector(landmarks[23]), ToVector(landmarks[24])),
+        };
+
+        if (hips.Rotation.y > .5f)
+            hips.Rotation.y -= 2;
+
+        hips.Rotation.y += .5f;
+
+        //Stop jumping between left and right shoulder tilt
+        if (hips.Rotation.z > 0)
+            hips.Rotation.z = 1 - hips.Rotation.z;
+
+        if (hips.Rotation.z < 0)
+            hips.Rotation.z = -1 - hips.Rotation.z;
+
+        float turnAroundAmountHips = Math.Abs(hips.Rotation.y).Remap(.2f, .4f);
+        hips.Rotation.z *= 1 - turnAroundAmountHips;
+        hips.Rotation.x = 0; // Temp fix for inaccurate X axis
+
+        Vector3 spine = VectorExtensions.RollPitchYaw(ToVector(landmarks[11]), ToVector(landmarks[12]));
+
+        if (spine.y > .5f)
+            spine.y -= 2;
+
+        spine.y += .5f;
+
+        // Prevent jumping between left and right shoulder tilt
+        if (spine.z > 0)
+            spine.z = 1 - spine.z;
+
+        if (spine.z < 0)
+            spine.z = -1 - spine.z;
+
+        // Fix weird large numbers when 2 shoulder points get too close
+        float turnAroundAmount = Math.Abs(spine.y).Remap(.2f, .4f);
+        spine.z *= 1 - turnAroundAmount;
+        spine.x = 0; // Temp fix for inaccurate X axis
+
+        RigHips(ref hips, ref spine);
+        return (hips, spine);
+    }
+
+    private static void RigHips(ref Hips hips, ref Vector3 spine)
+    {
+        // Convert normalized values to radians
+        hips.Rotation *= MathF.PI;
+
+        hips.WorldPosition = new Vector3
+        (
+            hips.Position.x * (.5f + 1.8f * -hips.Position.z),
+            0,
+            hips.Position.z * (.1f + hips.Position.z * -2)
+        );
+
+        spine *= MathF.PI;
+    }
+}
---
+++ VectorExtensions.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class VectorExtensions
+{
+    public static float Find2DAngle(float cx, float cy, float ex, float ey)
+    {
+        var dy = ey - cy;
+        var dx = ex - cx;
+        return MathF.Atan2(dy, dx);
+    }
+
+    public static Vector2 Unit(Vector2 vector) => vector / vector.magnitude;
+    public static Vector3 Unit(Vector3 vector) => vector / vector.magnitude;
+
+    public static float Remap(this float val, float min, float max) => (Math.Clamp(val, min, max) - min) / (max - min);
+
+    public static float NormalizeRadians(float radians)
+    {
+        if (radians >= MathF.PI / 2)
+            radians -= 2 * MathF.PI;
+
+        if (radians <= -MathF.PI / 2)
+        {
+            radians += 2 * MathF.PI;
+            radians = MathF.PI - radians;
+        }
+
+        return radians / MathF.PI;
+    }
+
+    public static float NormalizeAngle(this float radians)
+    {
+        var twoPi = MathF.PI * 2;
+        var angle = radians % twoPi;
+        angle = angle > MathF.PI ? angle - twoPi : angle < -MathF.PI ? twoPi + angle : angle;
+        return angle / MathF.PI;
+    }
+
+    public static Vector3 FindRotation(Vector3 vector, Vector3 other, bool normalize)
+    {
+        Vector3 result = new(
+            Find2DAngle(vector.z, vector.x, other.z, other.x),
+            Find2DAngle(vector.z, vector.y, other.z, other.y),
+            Find2DAngle(vector.x, vector.y, other.x, other.y)
+         );
+
+        if (normalize)
+        {
+            return result.normalized;
+        }
+
+        return result;
+    }
+
+    /// Find 2D angle between 3 points in 3D space. Returns a single angle normalized to 0, 1
+    public static float AngleBetween3DCoords(Vector3 a, Vector3 b, Vector3 c)
+    {
+        var vec1 = a - b;
+        var vec2 = c - b;
+
+        var vec1Norm = Unit(vec1);
+        var vec2Norm = Unit(vec2);
+
+        var dotProducts = Vector3.Dot(vec1Norm, vec2Norm);
+        var angle = MathF.Acos(dotProducts);
+
+        return NormalizeRadians(angle);
+    }
+
+    public static Vector3 RollPitchYaw(Vector3 a, Vector3 b, Vector3? c = null)
+    {
+        if (c == null)
+        {
+            return new Vector3(
+                Find2DAngle(a.z, a.y, b.z, b.y).NormalizeAngle(),
+                Find2DAngle(a.z, a.x, b.z, b.x).NormalizeAngle(),
+                Find2DAngle(a.x, a.y, b.x, b.y).NormalizeAngle()
+                );
+        }
+
+        var qb = b - a;
+        var qc = (Vector3)(c - a);
+        var n = Vector3.Cross(qb, qc);
+
+        var unitZ = Unit(n);
+        var unitX = Unit(qb);
+        var unitY = Vector3.Cross(unitZ, unitX);
+
+        var beta = MathF.Asin(unitZ.x);
+        var alpha = MathF.Atan2(-unitZ.y, unitZ.z);
+        var gamma = MathF.Atan2(-unitY.x, unitX.x);
+
+        return new Vector3(alpha.NormalizeAngle(), beta.NormalizeAngle(), gamma.NormalizeAngle());
+    }
+}
